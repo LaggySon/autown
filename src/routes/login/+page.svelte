4f67643d8bec7ec921c9cd86925f7db82c697376
Danny Dang <dannydang4012@gmail.com>
2023-03-14 18:29:15 -0400

    login screen v1

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<h1 class="text-center text-7xl">Autown</h1>
+
+<body>
+    <div id="Container" class="h-80 w-full flex flex-col items-center justify-center">
+        <button class= "bg-gray-300 hover:bg-gray-400 items-center p-2 text-black font-bold py-2 px-6">
+            Log In
+        </button>
+        <button class= "bg-gray-300 hover:bg-gray-400 items-center p-2 mt-2 text-black font-bold py-2 px-5">
+            Sign Up
+        </button>
+    </div>
+</body>
+
+
