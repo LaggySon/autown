626be70831ffad02e88d7cbd278edca008eb1e0f
Gabriel Baffo <baffog@wit.edu>
2023-04-05 13:08:04 -0400

    Added trip estimation functionality.

--- src/routes/tripmanager/+page.svelte
+++ src/routes/tripmanager/+page.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
+	/// <reference types="@types/google.maps" />
 	import Plus from '$lib/plus.svelte';
 	import Trip from './Trip.svelte';
 	import authStore from '../../stores/authStore';
 
 	export let data;
+
+	async function determineQuickestMode(origin: string, destination: string): Promise<string> {
+		var service = new google.maps.DistanceMatrixService();
+
+		const viaDriving = await service.getDistanceMatrix({
+			origins: [origin],
+			destinations: [destination],
+			travelMode: google.maps.TravelMode.DRIVING
+		});
+		const viaTransit = await service.getDistanceMatrix({
+			origins: [origin],
+			destinations: [destination],
+			travelMode: google.maps.TravelMode.TRANSIT
+		});
+
+		if (viaDriving.rows[0].elements[0].status == "OK" && viaTransit.rows[0].elements[0].status == "OK") {
+			var drivingTime = viaDriving.rows[0].elements[0].duration.value;
+			var transitTime = viaTransit.rows[0].elements[0].duration.value;
+
+			if (viaDriving < viaTransit) {
+				return 'Driving';
+			}
+			if (viaTransit < viaDriving) {
+				return 'Transit';
+			}
+			return 'Both!';
+		} else {
+			return "Error";
+		}
+	}
 
 	const getTrips = (email: string) => {
 		const userTrips = data.docs.find((doc: any) => doc.email === $authStore.user.email).trips;
@@ -12,11 +43,18 @@
 	};
 </script>
 
+<svelte:head>
+	<script
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_xH2-aYVJmgkmo9BazIL8XQZzG39OBTc"
+	></script>
+</svelte:head>
+
 <div class="m-auto w-3/4 ">
 	<h1 class="text-5xl text-center p-10  font-bold">My Trips</h1>
 	<div class="flex flex-wrap justify-center items-center mx-auto gap-4">
 		{#if $authStore.isLoggedIn}
 			{#each getTrips($authStore.user.email) as trip, i}
+				{determineQuickestMode("Boston, MA", "New York City, New York")}	
 				<a href="/trips/{i}"
 					><Trip name={trip.name} origin={trip.origin} dest={trip.destination} /></a
 				>
--- src/routes/tripmanager/Trip.svelte
+++ src/routes/tripmanager/Trip.svelte
@@ -4,33 +4,6 @@
 	export let dest: string;
 	import Train from '$lib/train.svelte';
 	import Car from '$lib/car.svelte';
-	import axios from 'axios';
-	import {Client, TravelMode} from "@googlemaps/google-maps-services-js";
-
-	// Make a request for a user with a given ID
-	const tripOrigin = "555 Huntington Avenue, Boston MA"
-	const tripDestination = "200 Commonwealth Ave, Boston MA"
-
-	const client = new Client({});
-	
-	client
-      .distancematrix({
-        params: {
-          key: '',
-          origins: ['Greenwich, England'],
-          destinations: ['Stockholm, Sweden'],
-          mode: TravelMode.driving,
-          // Feel free to set more params
-        },
-
-        timeout: 1000, // milliseconds
-      })
-      .then((r) => {
-        console.log(r.data.rows[0]?.elements);
-      })
-      .catch((e) => {
-        console.log(e.response.data.status);
-      });
 </script>
 
 <div
@@ -42,7 +15,7 @@
 		<p>Destination: {dest}</p>
 	</div>
 	<div class="flex justify-center items-center gap-4 font-bold text-lg p-2">
-		<Car fill="red" /> VS <Train fill="green" />
+		<span id="CarIcon"><Car fill="red"/></span> VS <span id="TrainIcon"><Train fill="green" /></span>
 	</div>
 	<div><span class="uppercase font-bold">train</span> is faster today!</div>
 </div>
